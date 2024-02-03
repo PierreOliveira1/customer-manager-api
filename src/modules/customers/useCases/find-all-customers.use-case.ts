e1d1e835b4244cd261f3cadac87226e568166006
Pierre Oliveira <pierre.torres445@gmail.com>
2024-02-03 05:52:45 -0300

    fix: pagination for find all customers

--- src/modules/customers/useCases/find-all-customers.use-case.ts
+++ src/modules/customers/useCases/find-all-customers.use-case.ts
@@ -16,27 +16,30 @@ export class FindAllCustomersUseCase {
 			const offset = (page - 1) * limit;
 
 			const query: string[] = [];
+			const countQuery: string[] = [];
 			const params: (number | string)[] = [];
+			const countParams: (number | string)[] = [];
+			countQuery.push('SELECT COUNT(id) AS total FROM customers c');
 			query.push(
 				'SELECT id, name, email, phone_number AS "phoneNumber", coordinate_x AS "coordinateX", coordinate_y AS "coordinateY" FROM customers c',
 			);
 
 			if (filters?.search) {
 				const id = params.length + 1;
-				query.push(
-					`WHERE c.name ILIKE $${id} OR c.email ILIKE $${id} OR c.phone_number ILIKE $${id}`,
-				);
-				params.push(`%${filters.search}%`);
+				const newQuery = `WHERE c.name ILIKE $${id} OR c.email ILIKE $${id} OR c.phone_number ILIKE $${id}`;
+				query.push(newQuery);
+				countQuery.push(newQuery);
+				const filter = `%${filters.search}%`;
+				params.push(filter);
+				countParams.push(filter);
 			}
 
 			query.push(`LIMIT $${params.length + 1} OFFSET $${params.length + 2}`);
 			params.push(limit, offset);
 
 			const [result, totalResult] = await Promise.all([
-				pool.query<Customer>(query.join(' '), [...params]),
-				pool.query<{ total: string }>(
-					'SELECT COUNT(id) AS total FROM customers',
-				),
+				pool.query<Customer>(query.join(' '), params),
+				pool.query<{ total: string }>(countQuery.join(' '), countParams),
 			]);
 
 			const total = parseInt(totalResult.rows[0].total, 10);
